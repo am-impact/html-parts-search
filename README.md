531f36172cb35bc7e3650efa853bc7769cf95446
Niek <n.weevers@am-impact.nl>
2013-11-18 15:08:51 +0100

    Begin

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 html-parts-search
 =================
+
+Uitbreiding op [html startup](https://github.com/nweevers/html_startup_inuit)
---
+++ scripts/yeah.search.js
@@ -0,0 +1,45 @@
+var Yeah = Yeah || {};
+
+$('.searchform form').validateSearch();
+
+/**
+ * Search form validation
+ */
+$.fn.validateSearch = function( options )
+{
+    return this.each(function(index, el)
+    {
+        var defaults = {
+            placeholderAttr : 'placeholder',
+            minValueLength  : 3,
+            messageTimeout  : 5000,
+            errorMessage    : 'Voer 3 of meer karakters in',
+            formErrorClass  : 'searcherror',
+            errorMessageClass : 'errormessage'
+        },
+        o = $.extend( defaults, options ),
+        $el = $(el);
+        inputField = $el.find('input[type=text]'),
+
+        $el.append('<div class="' + o.errorMessageClass + '">' + o.errorMessage + '</div>');
+
+        $el.on('submit', function(e)
+        {
+            var $this = $(this),
+                inputValue = $.trim( $this.find('input[type=text]').val() ),
+                inputValueLength = inputValue.length;
+
+            if( inputValueLength < o.minValueLength || inputValue == inputField.attr( o.placeholderAttr ) )
+            {
+                $this.addClass( o.formErrorClass );
+
+                setTimeout(function()
+                {
+                    $this.removeClass( o.formErrorClass );
+                }, o.messageTimeout);
+
+                e.preventDefault();
+            }
+        });
+    });
+};
